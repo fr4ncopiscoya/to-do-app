caabdec3111fa57e81fea083cacb139a708e680f
Franco Piscoya <114585682+fr4ncopiscoya@users.noreply.github.com>
2024-03-19 09:11:34 -0500

    feat(to-do-list): Se creo la app de to-do list, usando Flutter con lenguaje dart y db sqflite

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:apptodo/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home:HomeScreen(),
+    );
+  }
+}
+
+
